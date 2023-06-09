aef5ae550711a84c9606c03eaa336de4296cc8a7
mak9su4roi <makkusu.shiroi@gmail.com>
2023-06-09 10:22:42 +0100

    lock: Fix solenoid lock API

--- lock/solenoid/raspberry.py
+++ lock/solenoid/raspberry.py
@@ -11,12 +11,13 @@ class API:
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.RELAY_PIN, GPIO.OUT)
+        GPIO.output(self.RELAY_PIN, 1)
     
     def _open(self, duration: float):
         print("Oppened")
-        GPIO.output(self.RELAY_PIN, 1)
-        sleep(duration)
         GPIO.output(self.RELAY_PIN, 0)
+        sleep(duration)
+        GPIO.output(self.RELAY_PIN, 1)
     
     def open(self, duration: float):
         threading.Thread(target=lambda: self._open(duration=duration), daemon=True).start()
